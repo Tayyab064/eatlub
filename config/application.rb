8394dd5ca4e62ad0e8558ac42e6bff8b466733b7
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2021-01-01 01:50:46 +0500

    timezone

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Deliverish
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
-    config.time_zone = 'Dubai'
+    config.time_zone = 'London'
  	config.active_record.default_timezone = :local # Or :utc
   end
 end
